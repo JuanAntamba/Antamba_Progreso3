f7dc698bf2590b3f5b6fa150bd04cc23a8158eb2
Juan Antamba <juan.antamba@udla.edu.ec>
2024-01-23 15:34:45 -0500

    Se creo los dos case en donde se uso estrucutras para guardar la informacion de los nuevos alumnos, ademas se uso un archivo plano para guardar y mostrar la informacion

--- progreso 3Final.c
+++ progreso 3Final.c
@@ -1,38 +1,84 @@
 #include <stdio.h>
 
-
-int main (){
+int main() {
+    FILE *archivo;
     int opcion;
-    struct alumno
+
+    struct Alumno 
     {
-      int matricula;
-      char nombre [50];
-      char direccion [50];
-      char materia [50];
-      float nota;  
-    }
+    int matricula;
+    char nombre[50];
+    char direccion[50];
+    char materia[50];
+    float nota ;
+    };
 
-    int opcion;
 
 
     do {
-        printf("1.- Ingresa un alumno nuevo");
-        printf("2.- Ver los alumnos ingresados");
-        printf("3.- Salir");
-        printf("Seleccione una opcion");
+        printf("1.- Ingresar un alumno nuevo\n");
+        printf("2.- Mostrar alumnos ingresados\n");
+        printf("3.- Salir\n");
+        printf("Seleccione una opcion: ");
+        scanf("%d", &opcion);
 
-        scanf("%d",&opcion);
+        switch (opcion) {
+            case 1: {
+                struct Alumno alumnoIngresado;
 
+                printf("Ingrese la matrícula del alumno: ");
+                scanf("%d", &alumnoIngresado.matricula);
 
-    switch (opcion) {
-            case 1:  // Ingresar alumno 
-               
-            case 2:  //ver alumos
+                printf("Ingrese el nombre del alumno: ");
+                scanf("%s", alumnoIngresado.nombre);
 
-            case 3:  
-                printf("Eligio la opcion de salir");
+                printf("Ingrese la dirección del alumno: ");
+                scanf("%s", alumnoIngresado.direccion);
+
+                printf("Ingrese la materia del alumno: ");
+                scanf("%s", alumnoIngresado.materia);
+
+                printf("Ingrese la matrícula del alumno: ");
+                scanf("%f", &alumnoIngresado.nota);
+
+                
+                archivo = fopen("alumnos.txt", "a");// En esta parte abre el archivo en modo de escritura  o crea uno nuevo
+
+                fprintf(archivo, "%d %s %s %s %f\n", alumnoIngresado.matricula, alumnoIngresado.nombre, alumnoIngresado.direccion, alumnoIngresado.materia, alumnoIngresado.nota);// Escribe el alumno en el archivo
+
+                fclose(archivo);// Cierra el archivo
+
+                printf("Alumno ingresado con éxito.\n");
+                break;
+            }
+
+            case 2: {
+                struct Alumno alumno;
+
+                
+                archivo = fopen("alumnos.txt", "r");// Abre el archivo en modo de lectura para lo cualse usa r
+
+                if (archivo == NULL) {
+                    printf("No se puedo emcontrar el archivo\n");
+                    break;
+                }
+
+                
+                while (fscanf(archivo, "%d %s %s %s %f", &alumno.matricula, alumno.nombre, alumno.direccion, alumno.materia, alumno.nota) {
+
+                    printf("Matricula: %d, Nombre: %s, Direccion: %s, Materia: %s\n", alumno.matricula, alumno.nombre, alumno.direccion, alumno.materia,alumno.nota);// se muestra cada alumno del archivo
+                }
+
+                
+                fclose(archivo);// Se cierra el archivo
+                break;
+            }
+
+            case 3:
+                printf("Selecciono la opcion de salir\n");
                 break;
         }
+
     } while (opcion != 3);
 
     return 0;
