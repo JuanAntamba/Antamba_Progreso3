946e8fcccc9260b4fe0d957d7e1908340a4974d6
Juan Antamba <juan.antamba@udla.edu.ec>
2024-01-23 14:48:18 -0500

    Creacion del menu donde se le mostrara al usurio las opciones requeridas y mediante un swirch y los case de estos se ejecutara la ocpcion desada por el usuario

--- progreso 3Final.c
+++ progreso 3Final.c
@@ -1,10 +1,39 @@
 #include <stdio.h>
 
-struct alumno
-{
-    int matricula;
-    char nombre [50];
-    char direccion [50];
-    char materia [50];
-    float nota;
-};
+
+int main (){
+    int opcion;
+    struct alumno
+    {
+      int matricula;
+      char nombre [50];
+      char direccion [50];
+      char materia [50];
+      float nota;  
+    }
+
+    int opcion;
+
+
+    do {
+        printf("1.- Ingresa un alumno nuevo");
+        printf("2.- Ver los alumnos ingresados");
+        printf("3.- Salir");
+        printf("Seleccione una opcion");
+
+        scanf("%d",&opcion);
+
+
+    switch (opcion) {
+            case 1:  // Ingresar alumno 
+               
+            case 2:  //ver alumos
+
+            case 3:  
+                printf("Eligio la opcion de salir");
+                break;
+        }
+    } while (opcion != 3);
+
+    return 0;
+}
